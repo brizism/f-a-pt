f440e4a7a9b61137fc915d4e706bb8ee9598989a
brizism <montielbriseida@gmail.com>
2018-07-06 00:09:12 -0400

    paint data

--- js/index.js
+++ js/index.js
@@ -30,7 +30,28 @@ function showAnimals(pets) {
 
   //Loop through pets
   pets.forEach(pet => {
-    console.log(pet)
+    console.log(pet);
+    const div = document.createElement('div');
+    div.classList.add('card', 'card-body', 'mb-3');
+    div.innerHTML = `
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>${pet.name.$t} (${pet.age.$t})</h4>
+          <p class="text-secondary">${pet.breeds.breed.$t}</p>
+          <p>${pet.contact.address1.$t} ${pet.contact.city.$t} ${pet.contact.state.$t} ${pet.contact.zip.$t}</p>
+          <ul class="list-group">
+            <li class="list-group-item">Phone: ${pet.contact.phone.$t}</li>
+            ${pet.contact.email.$t ? `<li class="list-group-item">Email: ${pet.contact.email.$t}</li>` : ``}
+            <li class="list-group-item">Shelter ID: ${pet.shelterId.$t}</li>
+          </ul>
+        </div>
+        <div class="col-sm-6 text-center">
+          <img class="img-fluid rounded-circle mt-2" src="${pet.media.photos.photo[3].$t}" alt=""/>
+        </div>
+      </div>
+    `;
+
+    results.appendChild(div);
   })
 
 }
